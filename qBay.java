e447ba1ba607d105867d1555c0a14105b86005a5
jlafrance3 <jlafranceiii@proton.me>
2024-03-27 19:19:53 -0400

    Added welcome message and code header

---
+++ qBay.java
@@ -0,0 +1,20 @@
+/*
+ * CSC-109 Challenge1
+ * Peter Zegarek, Connor Nylund, Jean LaFrance
+ * Proof of concept program of a marketplace application to
+ * allow buying/selling of items
+ */
+
+
+public class qBay {
+
+    public static void main(String args[]) {
+
+        System.out.println("\nWelcome to Quinnipiac Marketplace!");
+        System.out.println(" ______     ______     ______     __  __    \n/\\  __ \\   " + 
+        "/\\  == \\   /\\  __ \\   /\\ \\_\\ \\   \n\\ \\ \\/\\_\\  \\ \\  __<   \\ \\  __ " + 
+        "\\  \\ \\____ \\  \n \\ \\___\\_\\  \\ \\_____\\  \\ \\_\\ \\_\\  \\/\\_____\\ \n  " + 
+        "\\/___/_/   \\/_____/   \\/_/\\/_/   \\/_____/ \n                                            ");
+        System.out.println("For all of your buying and selling needs");
+    }
+}
